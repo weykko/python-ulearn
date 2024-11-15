b0b407a3ff948fabda3598df1a15c31262bbccad
aleksandr <beznutrovs@gmail.com>
2024-11-15 22:08:49 +0500

    task 9.3 changed

--- 09. Анализ данных/task 9.3.py
+++ 09. Анализ данных/task 9.3.py
@@ -7,15 +7,15 @@ def get_city_salary_stats(vacancies):
     filtered_vacancies = vacancies[vacancies['salary_currency'] == 'RUR'].copy()
     filtered_vacancies['average_salary'] = filtered_vacancies[['salary_from', 'salary_to']].mean(axis=1)
 
-    city_salary = (filtered_vacancies
+    city_salary = ((filtered_vacancies
         .groupby('area_name')['average_salary']
         .mean()
         .dropna()
         .round()
         .astype(int)
         .sort_index()
-        .sort_values(ascending=False, kind='stable')
-        ).to_dict()
+        .sort_values(ascending=False, kind='stable'))
+        .to_dict())
 
     return city_salary
 
